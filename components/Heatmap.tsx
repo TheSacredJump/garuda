e93fb2b6523c1d84f879e538b76ae7e63b750feb
TheSacredJump <padhu.sathya@gmail.com>
2024-10-12 16:40:36 -0400

    heatmap quick change

--- components/Heatmap.tsx
+++ components/Heatmap.tsx
@@ -6,13 +6,14 @@ const mapContainerStyle = {
   height: '600px'
 };
 
-const center = {
-  lat: 30,
-  lng: -40
+const floridaCenter = {
+  lat: 27.6648,
+  lng: -81.5158
 };
 
 const HurricaneHotzoneComponent = () => {
   const [hotzoneData, setHotzoneData] = useState([]);
+  const [mapRef, setMapRef] = useState(null);
 
   useEffect(() => {
     const fetchHotzoneData = async () => {
@@ -28,14 +29,23 @@ const HurricaneHotzoneComponent = () => {
     fetchHotzoneData();
   }, []);
 
+  const onMapLoad = (map) => {
+    setMapRef(map);
+    const bounds = new window.google.maps.LatLngBounds();
+    bounds.extend(new window.google.maps.LatLng(24.396308, -87.634896)); // Southwest corner of Florida
+    bounds.extend(new window.google.maps.LatLng(31.000888, -79.974306)); // Northeast corner of Florida
+    map.fitBounds(bounds);
+  };
+
   return (
     <LoadScript
       googleMapsApiKey={`${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}
     >
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
-        center={center}
-        zoom={3}
+        center={floridaCenter}
+        zoom={7}
+        onLoad={onMapLoad}
       >
         {hotzoneData.map((hotzone, index) => (
           <CircleF
@@ -44,7 +54,7 @@ const HurricaneHotzoneComponent = () => {
               lat: hotzone.location.lat,
               lng: hotzone.location.lng
             }}
-            radius={hotzone.radius * 10000} // Adjust this multiplier to make circles more visible
+            radius={hotzone.radius * 1000} // Adjust this multiplier to make circles more visible
             options={{
               fillColor: hotzone.color,
               fillOpacity: 0.7,
